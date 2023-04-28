dd7b0efc68e30b0f9b5da363f278222c232acce4
StacyKangethe <stacykangethe18@gmail.com>
2023-04-28 09:01:22 +0300

    Add Logo

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
     return (
         <div>
             <footer>
-                <p>Copyright &copy; Glass Recycling Management System Webapp</p>
+                <p>Copyright &copy; Print Patrol 2023</p>
             </footer>
         </div>
     );
